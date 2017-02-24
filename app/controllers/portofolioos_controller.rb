943a9f43902d87481af99cdf8640c64662663308
DeanDK <deanbozic12@gmail.com>
2017-02-24 12:11:27 +0000

    New and create actions for portofolio

--- app/controllers/portofolioos_controller.rb
+++ app/controllers/portofolioos_controller.rb
@@ -3,4 +3,21 @@ class PortofolioosController < ApplicationController
   def index
     @portofolios_items = Portofolioo.all
   end
+  
+  def new
+    @portofolios_items = Portofolioo.new
+  end
+  
+   def create
+    @portofolios_items = Portofolioo.new(params.require(:portofolioo).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portofolios_items.save
+        format.html { redirect_to portofolioos_path, notice: 'Portofolioo was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+  
 end
