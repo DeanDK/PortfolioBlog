364944c0d6566fc0fbd7460ed1aaa8dafeb148e3
DeanDK <deanbozic12@gmail.com>
2017-03-01 22:52:38 +0000

    Implemented logut, login and register

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
   resources :portofolioos, except: [:show]
   get 'portofolioo/:id', to: 'portofolioos#show', as: 'portofolioo_show'
   get 'angular-items', to: 'portofolioos#angular'
+  
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
